732b4adbd7f6693508bf3fe38025be194731b313
Ewelina Wasilewski <ewelina.wasilewski@gmail.com>
2014-09-03 21:57:18 +0200

    Autowired root controller

--- src/main/java/org/pmt/web/RootController.java
+++ src/main/java/org/pmt/web/RootController.java
@@ -1,15 +1,30 @@
 package org.pmt.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RootController {
 
+    private CustomerDao customerDao;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String getRoot() {
         return "index";
+    }
+
+    @Autowired
+    public void setCustomerDao(CustomerDao customerDao) {
+        this.customerDao = customerDao;
+    }
+
+    public CustomerDao getCustomerDao() {
+        return customerDao;
     }
 
 }
